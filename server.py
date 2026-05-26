8ab25609b49ca8cb2a2cb1cd3aea403cb8d5d4a6
bcwq <95038052+Wa111ng@users.noreply.github.com>
2026-05-26 17:49:18 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,26 @@
-from fastmcp import FastMCP
-from fastmcp.server.dependencies import get_access_token
+from datetime import datetime
+import os
 
-mcp = FastMCP(name="Google Secure MCP")
+from mcp.server.fastmcp import FastMCP
 
-@mcp.tool()
-async def who_am_i() -> dict:
-    token = get_access_token()
-
-    return {
-        "user_id": token.claims.get("sub"),
-        "email": token.claims.get("email"),
-        "name": token.claims.get("name"),
-    }
+PORT = os.environ.get("PORT", 8000)
+mcp = FastMCP("Frontwave", host="0.0.0.0", port=PORT)
 
 
-@mcp.tool()
-def add(a: float, b: float) -> float:
+@mcp.tool(description="Add 2 numbers")
+def add_numbers(a: float, b: float) -> float:
     return a + b
+
+
+@mcp.tool(description="Returns current time")
+def current_time() -> str:
+    return datetime.utcnow().isoformat()
+
+
+@mcp.tool(description="A simple echo tool")
+def echo(message: str) -> str:
+    return f"Frontwave says: {message}"
+
+
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http")
